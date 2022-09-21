b12aa64353f63fa6a46fc3bc655b263c74814514
JoshG <joshcode25@gmail.com>
2022-09-21 06:32:37 -0500

    clarified allowablemoves and added learnedmoves

--- pokemon-rumble/src/Components/getNewPokemon.js
+++ pokemon-rumble/src/Components/getNewPokemon.js
@@ -7,35 +7,42 @@ async function getNewPokemon(identifier, level) {
     const url = `https://pokeapi.co/api/v2/pokemon/${identifier}`; //adjusts url based on id given
     const response = await fetch(url);
     const data = await response.json();
+    newPokemon.data = data;
     newPokemon.name = data.name;
     newPokemon.id = data.id;
     newPokemon.baseExperience = data.base_experience;
     newPokemon.currentExperience = Math.pow(newPokemon.level, 3);
 
     //get moves
-    const movesList = await Promise.all(data.moves.map(async function (move) {
+    newPokemon.allowableMovesList = await Promise.all(data.moves.map(async function (move) {
       const moveUrl = move.move.url; //gets move url from pokemon info
       const moveDataResponse = await fetch(moveUrl); //fetch move info
       const moveData = await moveDataResponse.json(); //convert move info to json
       return moveData;
     }))
-    console.log(movesList);
-    //   const levelLearned = move.version_group_details[0].level_learned_at; //get info when the pokemon learns the move
 
-    //   const moveInfo = {
-    //     //consolidates move info to add to array
-    //     name: moveData.name,
-    //     levelLearnedAt: parseInt(levelLearned, 10),
-    //     accuracy: parseInt(moveData.accuracy, 10),
-    //     power: parseInt(moveData.power, 10),
-    //     type: moveData.type.name,
-    //     damageClass: moveData.damage_class.name,
-    //   };
-    //   // newPokemon.availableMovesList.push(moveInfo);
-    //   if (levelLearned !== 0) {
-    //     //only adds moves the pokemon will learn naturally by leveling up
-    //     newPokemon.learnedMovesList.push(moveInfo);
-    //   }
+    //reduce to only moves learned naturally
+    newPokemon.learnedMovesList = data.moves.reduce((runningList, move) => {
+    
+      const levelLearned = move.version_group_details[0].level_learned_at; //get info when the pokemon learns the move
+
+      if (levelLearned !== 0) {
+
+          const moveInfo = { //consolidates move info to add to array
+          name: move.move.name,
+          levelLearnedAt: parseInt(levelLearned, 10),
+          // accuracy: parseInt(move.accuracy, 10),
+          // power: parseInt(move.power, 10),
+          // type: move.type.name,
+          // damageClass: move.damage_class.name,
+          };
+
+          runningList.push(moveInfo);
+      }
+      
+      return runningList;
+  }, [])
+
     //   if (newPokemon.level >= levelLearned && levelLearned !== 0) {
     //     //adds moves to current moves depending on pokemon's level
     //     newPokemon.currentMovesList.push(moveInfo);
